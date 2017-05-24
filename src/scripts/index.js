5dd69f42ed45d31dea727266b02c1398c8a8483f
arellaTV <jeremiah.arella@gmail.com>
2017-05-24 13:41:29 -0700

    Rename .js to .jsx extension to allow for ES6 backticks

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,6 @@
 // POLYFILLS
 if (!Array.forEach) Array.forEach = require('array-foreach');
-NodeList.prototype.forEach = Array.prototype.forEach; 
+NodeList.prototype.forEach = Array.prototype.forEach;
 HTMLCollection.prototype.forEach = Array.prototype.forEach; // Because of https://bugzilla.mozilla.org/show_bug.cgi?id=14869
 if (!Array.from) Array.from = require('array-from');
 
@@ -8,5 +8,5 @@ if (!Array.from) Array.from = require('array-from');
 // REQUIRE MODULES
 require('./media.js');
 require('./wells.js');
-require('./mast.js');
-require('svgxuse'); 
+require('./mast.jsx');
+require('svgxuse');
